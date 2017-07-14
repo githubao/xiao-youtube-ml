4bfa5ce9cdfcd070c5da973939ab7cf7309a555c
BaoQiang <mailbaoqiang@gmail.com>
2017-07-14 19:47:06 +0800

    cnn

---
+++ awesomeml/ch02/sec13_tflearn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@description: 
+
+@author: BaoQiang
+@time: 2017/7/14 19:45
+"""
+
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+
+
+def run():
+    pass
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
