3a09d6a52fc1cd6c6ab23ab66ce93840a311fe14
BaoQiang <mailbaoqiang@gmail.com>
2017-07-17 13:43:57 +0800

    ai playing games

--- awesomeml/ch02/sec14_gym.py
+++ awesomeml/ch02/sec14_gym.py
@@ -89,8 +89,82 @@ def initial_population():
     return training_data
 
 
+def neural_network_model(input_size):
+    network = input_data(shape=[None, input_size, 1], name='input')
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, keep_prob=0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, keep_prob=0.8)
+
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, keep_prob=0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, keep_prob=0.8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, keep_prob=0.8)
+
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+
+
+def train_model(training_data, model=False):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1)
+    y = np.array([i[1] for i in training_data])
+
+    if not model:
+        model = neural_network_model(input_size=len(X[0]))
+
+    model.fit({'input': X}, {'target': y}, n_epoch=3, snapshot_step=500, show_metric=True, run_id='openaistuff')
+    return model
+
+
+def play():
+    train_data = initial_population()
+    model = train_model(train_data)
+
+    scores = []
+    choices = []
+
+    for each_game in range(10):
+        score = 0
+        game_memory = []
+        prev_obs = []
+        env.reset()
+
+        for _ in range(goal_steps):
+            env.render()
+
+            if len(prev_obs) == 0:
+                action = random.randrange(0, 2)
+            else:
+                action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
+
+            choices.append(action)
+            new_observation, reward, done, info = env.step(action)
+            prev_obs = new_observation
+            game_memory.append([new_observation, action])
+
+            score += reward
+
+            if done:
+                break
+
+        scores.append(score)
+
+    print('Average Score', sum(scores) / len(scores))
+    print('Choice 1: {}, Choice 0: {}'.format(choices.count(1) / len(choices), choices.count(1) / len(choices)))
+
+
 def run():
-    initial_population()
+    play()
 
 
 def main():
